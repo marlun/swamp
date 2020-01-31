4ed74b694c74d80b63576aab56f88ab078a710a8
Martin Lundberg <martin.lundberg@gmail.com>
2020-01-31 12:47:25 +0100

    Fake change in store

--- stores/store.js
+++ stores/store.js
@@ -4,6 +4,11 @@ function store (state, bus) {
     { text: 'More Mushrooms!', latlng: { lat: 56.06063205928058, lng: 14.615049366684604 } }
   ]
 
+  setTimeout(function () {
+    state.places.push({ text: 'Mushrooms!', latlng: { lat: 56.031005763355395, lng: 14.622974395751955 } })
+    bus.emit('render')
+  }, 1000)
+
   bus.on('map:marker:add', onMapMarkerAdd)
 
   function onMapMarkerAdd (place) {
